99c0ad81dc0ce6f495bfb52b2fca8e8db089e530
andyxzdev <andrewsgamer123@outlook.com>
2026-03-23 11:38:12 -0300

    first commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+// import { Globe, Smartphone, PenTool, Server } from "lucide-react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className=" w-full h-full text-center items-center justify-center text-black">
+        teste
+      </h1>
+    </main>
+  );
+};
+
+export default App;
